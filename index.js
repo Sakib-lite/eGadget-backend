68fe893261a0122299ef165f8927e0153fb8948b
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-07-23 21:08:51 +0600

    edited

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./utils/db');
 require('dotenv').config();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 const laptopRoutes = require('./server/routes/laptopRoutes');
 const mobileRoutes = require('./server/routes/mobileRoutes');
@@ -55,6 +55,6 @@ server.use('/api/order', orderRoutes);
 
 server.use(errorHandler);
 
-server.listen(PORT, () => {
+server.listen(PORT, function () {
   console.log(`> Server running at http://localhost:${PORT}`);
 });
