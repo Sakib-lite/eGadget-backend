c41a76b9f4fbe52e3518ceddccd3482ccec12fcc
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-07-23 20:40:28 +0600

    edited

--- server/server.js
+++ server/server.js
@@ -1,33 +1,61 @@
-const db = require('../utils/db');
+const express = require('express');
+const db = require('./../utils/db');
 require('dotenv').config();
-const app = require('./../app');
 const PORT = process.env.PORT || 3001;
-// const dev = process.env.NODE_ENV !== 'production';
+const cors = require('cors');
+const laptopRoutes = require('./routes/laptopRoutes');
+const mobileRoutes = require('./routes/mobileRoutes');
+const userRoutes = require('./routes/userRoutes');
+const reviewRoutes = require('./routes/reviewRoutes');
+const othersRoutes = require('./routes/othersRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+const errorHandler = require('./controllers/errorController');
+// const session = require('express-session')
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const orderController = require('./controllers/orderController');
+// const Error = require('./../utils/appError');
 
-process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION!');
-  console.log(err.name, err.message);
-  process.exit(1);
-});
-
+const server = express();
 db.connect();
+server.use(
+  cors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  })
+);
 
-const server = app.listen(PORT, (err) => {
+server.post(
+  '/webhook-checkout',
+  bodyParser.raw({ type: 'application/json' }),
+  orderController.webhookCheckout
+);
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(cookieParser());
+
+//middlewares
+//server.get will only be called for GET requests
+//server.use only see whether url starts with specified path;
+//server.all will match complete path.
+
+// process.env.NODE_ENV === 'development' ? server.use(morgan('dev')) : '';
+// server.use(helmet());
+
+//routes
+server.use('/api/product/laptop', laptopRoutes);
+server.use('/api/product/mobile', mobileRoutes);
+server.use('/api/product/other', othersRoutes);
+server.use('/api/users', userRoutes);
+server.use('/api/search', searchRoutes);
+server.use('/api/review', reviewRoutes);
+server.use('/api/order', orderRoutes);
+
+server.use(errorHandler);
+
+server.listen(PORT, (err) => {
   if (err) throw err;
   console.log(`> Server running at http://localhost:${PORT}`);
-});
-
-process.on('unhandledRejection', (err) => {
-  console.log('UNHANDLED REJECTION!');
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on('SIGTERM', () => {
-  console.log('SIGTERM RECEIVED.');
-  server.close(() => {
-    console.log('Process terminated!');
-  });
 });
