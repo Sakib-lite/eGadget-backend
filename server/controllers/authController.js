01531fe7b9360860033c2f686499ca58307cc6f2
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-07-24 20:42:11 +0600

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -20,9 +20,7 @@ const createTokenAndSendIt = (user, statusCode, res, message = 'Completed') => {
     expires: new Date(
       Date.now() + process.env.JWT_EXPIRES * 24 * 60 * 60 * 1000
     ),
-    domain: "vercel.app",
-    secure: true,
-    sameSite:'none',
+    domain: "vercel.app"
   });
   res.status(statusCode).json({
     status: 'success',
