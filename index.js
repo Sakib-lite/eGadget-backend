78bfb844972c8130d9f0ffb22d49812ba7da0959
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-07-24 18:29:49 +0600

    Update index.js

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ app.use(
     credentials: true,
   })
 );
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://e-gadget.vercel.app');
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
   next();
 });
 app.post(
