5da492ac49421061d58986eefd86d3f2d957fa39
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-07-27 01:55:43 +0600

    Update index.js

--- index.js
+++ index.js
@@ -26,16 +26,37 @@ app.use(
     origin: '*',
   })
 );
-app.use(
-  "/webhook-checkout",
-  express.json({
-    verify: (req, res, buf) => {
-      req.rawBody = buf.toString();
-    },
-  })
-);
-app.use(express.json());
-app.post('/webhook-checkout', orderController.webhookCheckout);
+
+
+app.use((req, res, next) => {
+  if (req.originalUrl === '/webhook') {
+    next();
+  } else {
+    express.json()(req, res, next);
+  }
+});
+
+// Stripe requires the raw body to construct the event
+app.post('/webhook', express.raw({type: 'application/json'}), (req, res) => {
+  const sig = req.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+  } catch (err) {
+    // On error, log and return the error message
+    console.log(`❌ Error message: ${err.message}`);
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  // Successfully constructed event
+  console.log('✅ Success:', event.id);
+
+  // Return a response to acknowledge receipt of the event
+  res.json({received: true});
+});
+
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
