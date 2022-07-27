37b24d24acfa7e1f640c0b61ed051750b01e43d8
Sakibul Anwar <77607002+Sakib-lite@users.noreply.github.com>
2022-07-28 00:03:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 app.use(
   cors({
     credentials: true,
-    origin: ['https://e-gadget.vercel.app','localhost:3001'],
+    origin: ['https://e-gadget.vercel.app','http://localhost:3001'],
   })
   );
 
